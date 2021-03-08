d8e9940d8161ab1d68e3710611ffb8e8039bc0b7
myoan <motoki.yoan@gmail.com>
2021-03-08 10:36:02 +0900

    Divide function

--- src/client/Cell.tsx
+++ src/client/Cell.tsx
@@ -10,31 +10,36 @@ export type CellProps = {
     onClick: (v: any) => void
 };
 
+function InsertEditableCell(x: number, y: number, width: number, height: number, text: string): HTMLTextAreaElement {
+    let canvas    = document.getElementsByClassName('canvas')[0];
+    let canvasPos = canvas.getBoundingClientRect();
+    let input     = document.getElementById('input-layer');
+    let textarea  = document.createElement('textarea');
+    if (input === null) { return textarea; }
+
+    input.appendChild(textarea);
+    textarea.value              = text;
+    textarea.style.position     = 'absolute';
+    textarea.style.top          = (canvasPos.y + y) + 'px';
+    textarea.style.left         = (canvasPos.x + x) + 'px';
+    textarea.style.width        = (width - 10 /*padding*/) + 'px';
+    textarea.style.height       = (height - 6)+ 'px';
+    textarea.style.fontSize     = '20px';
+    textarea.style.fontFamily   = 'Arial';
+    textarea.style.border       = 'none';
+    textarea.style.padding      = '3px 5px';
+    textarea.style.margin       = '0px';
+    textarea.style.overflow     = 'hidden';
+    textarea.style.background   = 'white';
+    textarea.style.outlineColor = 'green';
+    textarea.style.resize       = 'none';
+
+    return textarea;
+}
+
 const Cell: React.FC<CellProps> = props => {
     useEffect(() => {
-        let canvas    = document.getElementsByClassName('canvas')[0];
-        let canvasPos = canvas.getBoundingClientRect();
-        let input     = document.getElementById('input-layer');
-        let textarea  = document.createElement('textarea');
-        if (input === null) { return; }
-
-        input.appendChild(textarea);
-        textarea.value              = props.text;
-        textarea.style.position     = 'absolute';
-        textarea.style.top          = (canvasPos.y + props.y) + 'px';
-        textarea.style.left         = (canvasPos.x + props.x) + 'px';
-        textarea.style.width        = (props.width - 10 /*padding*/) + 'px';
-        textarea.style.height       = (props.height - 6)+ 'px';
-        textarea.style.fontSize     = '20px';
-        textarea.style.fontFamily   = 'Arial';
-        textarea.style.border       = 'none';
-        textarea.style.padding      = '3px 5px';
-        textarea.style.margin       = '0px';
-        textarea.style.overflow     = 'hidden';
-        textarea.style.background   = 'white';
-        textarea.style.outlineColor = 'green';
-        textarea.style.resize       = 'none';
-
+        let textarea = InsertEditableCell(props.x, props.y, props.width, props.height, props.text);
         textarea.addEventListener('keydown', (e) => {
             if (e.keyCode === 13) {
                 console.log(textarea.value);
@@ -61,13 +66,6 @@ const Cell: React.FC<CellProps> = props => {
                 x={props.x + 5}
                 y={props.y + 5}
                 fontSize={20}
-            />
-            <Rect
-                x={props.x}
-                y={props.y}
-                width={props.width}
-                height={props.height}
-                fill='rgba(0, 0, 0, 0)'
             />
         </Group>
     )
