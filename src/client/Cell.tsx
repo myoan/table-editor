9d144e2a1e4831246cf674735ec249cb92956e6f
myoan <motoki.yoan@gmail.com>
2021-03-08 10:15:03 +0900

    Use useEffect

---
+++ src/client/Cell.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import {Group, Rect, Text} from 'react-konva';
+
+export type CellProps = {
+    x: number
+    y: number
+    width: number
+    height: number
+    text: string
+    onClick: (v: any) => void
+};
+
+const Cell: React.FC<CellProps> = props => {
+    const [editable, setEditable] = useState(false);
+    useEffect(() => {
+        console.log('cell clicked');
+        let canvas = document.getElementsByClassName('canvas')[0];
+        let canvasPos = canvas.getBoundingClientRect();
+        let input = document.getElementById('input-layer');
+        let textarea = document.createElement('textarea');
+        if (input === null) { return; }
+
+        input.appendChild(textarea);
+        textarea.value = props.text;
+        textarea.style.position = 'absolute';
+        textarea.style.top = (canvasPos.y + props.y) + 'px';
+        textarea.style.left = (canvasPos.x + props.x) + 'px';
+        textarea.style.width = (props.width - 10 /*padding*/) + 'px';
+        textarea.style.height = (props.height - 6)+ 'px';
+        textarea.style.fontSize = '20px';
+        textarea.style.fontFamily = 'Arial';
+        textarea.style.border = 'none';
+        textarea.style.padding = '3px 5px';
+        textarea.style.margin = '0px';
+        textarea.style.overflow = 'hidden';
+        textarea.style.background = 'white';
+        textarea.style.outlineColor = 'green';
+        textarea.style.resize = 'none';
+
+        textarea.addEventListener('keydown', (e) => {
+            if (e.keyCode === 13) {
+                console.log(textarea.value);
+                props.onClick(textarea.value);
+                let input = document.getElementById('input-layer');
+                if (input === null) { return; }
+
+                while (input.firstChild) { input.removeChild(input.firstChild); }
+            }
+        });
+    })
+
+    return (
+        <Group>
+            <Rect
+                x={props.x}
+                y={props.y}
+                width={props.width}
+                height={props.height}
+                fill='#f0f0f0'
+            />
+            <Text
+                text={props.text}
+                x={props.x + 5}
+                y={props.y + 5}
+                fontSize={20}
+            />
+            <Rect
+                x={props.x}
+                y={props.y}
+                width={props.width}
+                height={props.height}
+                fill='rgba(0, 0, 0, 0)'
+                onClick={() => {
+                    console.log('cell clicked!!');
+                    setEditable(true);
+                }}
+            />
+        </Group>
+    )
+}
+
+export default Cell;
